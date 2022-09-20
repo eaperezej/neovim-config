9f15b203af1a805b9a7d093834d88ad3bbe65ff2
eaperezej <eaperezej@gmail.com>
2022-09-20 01:54:58 -0300

    no background

--- lua/theme.lua
+++ lua/theme.lua
@@ -11,3 +11,20 @@ vim.cmd [[
   syntax enable
   colorscheme catppuccin
 ]]
+
+local highlights = {
+  'Normal',
+  'NormalNC',
+  'Comment',
+  'LineNr',
+  'Folded',
+  'NonText',
+  'SpecialKey',
+  'VertSplit',
+  'SignColumn',
+  'EndOfBuffer'
+}
+
+for _, highlight in ipairs(highlights) do
+  vim.cmd(string.format("hi %s ctermbg=NONE guibg=NONE", highlight))
+end
